dcf99a15695f8306541789defd444f8d202874c5
ataniguchi <ataniguchi@vailsys.com>
2022-09-25 18:26:01 -0500

    Update discord.js

--- bot.js
+++ bot.js
@@ -1,20 +1,27 @@
 require('dotenv').config();
 const axios = require('axios');
-
 const Discord = require('discord.js');
-const client = new Discord.Client();
 
-const KANYE_NAMES = ['yeezy', 'yeezus', 'ye', 'kanye', 'mr west'];
+const client = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.MessageContent,
+    Discord.GatewayIntentBits.DirectMessages,
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages
+  ]
+});
+
+const KANYE_NAMES = ['yeezy', 'yeezus', 'ye', 'kanye'];
 
 client.on('ready', () => {
   console.log(`Logged in as ${ client.user.tag }`);
 })
 
-client.on('message', msg => {
+client.on('messageCreate', msg => {
   const message = msg.content.toLowerCase();
   const words = message.split(' ');
 
-  for(name of KANYE_NAMES) {
+  for(const name of KANYE_NAMES) {
     if(words.includes(name)) {
       axios.get('https://api.kanye.rest')
         .then(response => {
